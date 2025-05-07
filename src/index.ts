4e8e8e52fdcf84170d984bcde4966e26f65e4294
Ivet Lopez <ivet19@live.com>
2025-05-07 23:27:36 +0200

    Create handleHealthCheckStatus middleware and add endpoint on app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import startServer from "./server/startServer.js";
 
-const port = process.env.PORT || 4005;
+const port = process.env.PORT ?? 4005;
 
 startServer(Number(port));
