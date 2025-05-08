724643cc1113ce3e3316977ab23d72ae4a55fbf4
Ivet Lopez <ivet19@live.com>
2025-05-08 12:09:16 +0200

    Add endpointNotFound endpoint

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import handleHealthCheckStatus from "./middlewares/handleHealthCheckStatus/handleHealthCheckStatus.js";
 import handleErrors from "./middlewares/handleErrors/handleErrors.js";
+import handleEndpointNotFound from "./middlewares/handleEndpointNotFound/handleEndpointNotFound.js";
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.disable("x-powered-by");
 app.use(morgan("dev"));
 
 app.get("/", handleHealthCheckStatus);
+
+app.use(handleEndpointNotFound);
 
 app.use(handleErrors);
 
