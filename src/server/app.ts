47c6223cda1fad48377b024b63a91287415902e2
Ivet Lopez <ivet19@live.com>
2025-05-09 11:18:46 +0200

    Add get /restaurants endpoint to app

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,6 +3,8 @@ import morgan from "morgan";
 import handleHealthCheckStatus from "./middlewares/handleHealthCheckStatus/handleHealthCheckStatus.js";
 import handleErrors from "./middlewares/handleErrors/handleErrors.js";
 import handleEndpointNotFound from "./middlewares/handleEndpointNotFound/handleEndpointNotFound.js";
+import RestaurantController from "../restaurant/controller/RestaurantController.js";
+import Restaurant from "../restaurant/model/Restaurant.js";
 
 const app = express();
 
@@ -11,6 +13,10 @@ app.disable("x-powered-by");
 app.use(morgan("dev"));
 
 app.get("/", handleHealthCheckStatus);
+
+const controller = new RestaurantController(Restaurant);
+
+app.get("/restaurants", controller.getRestaurants);
 
 app.use(handleEndpointNotFound);
 
