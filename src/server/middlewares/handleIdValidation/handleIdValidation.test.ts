699a2879450a69edd4ae9d6297d1cdf551716fe5
Ivet Lopez <ivet19@live.com>
2025-05-16 12:15:12 +0200

    Add handleIdValidation tests

---
+++ src/server/middlewares/handleIdValidation/handleIdValidation.test.ts
@@ -0,0 +1,32 @@
+import { Response, NextFunction } from "express";
+import statusCodes from "../../../globals/statusCodes.js";
+import { RestaurantRequest } from "../../../restaurant/controller/types.js";
+import handleIdValidation from "./handleIdValidation.js";
+import ServerError from "../../ServerError/ServerError.js";
+
+describe("Given the handleValidationId middleware", () => {
+  describe("When it receives a request with a '123456789101213161718192' not valid restaurant id", () => {
+    const res = {} as Response;
+
+    const next = jest.fn();
+
+    test("Then it should call the received next method with code 400 and 'Not valid id' message", async () => {
+      const expectedError = new ServerError(
+        statusCodes.BAD_REQUEST,
+        "Not valid id",
+      );
+
+      const req: Pick<RestaurantRequest, "params"> = {
+        params: { restaurantId: "12345678910121316171819x" },
+      };
+
+      await handleIdValidation(
+        req as RestaurantRequest,
+        res as Response,
+        next as NextFunction,
+      );
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+});
