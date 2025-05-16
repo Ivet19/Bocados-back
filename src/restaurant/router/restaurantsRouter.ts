5654daa0d67cae8c77af29b425223cf55a2ce382
Ivet Lopez <ivet19@live.com>
2025-05-16 12:14:41 +0200

    Add id validation middleware to PATCH endpoint router

--- src/restaurant/router/restaurantsRouter.ts
+++ src/restaurant/router/restaurantsRouter.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import RestaurantController from "../controller/RestaurantController.js";
 import Restaurant from "../model/Restaurant.js";
+import handleIdValidation from "../../server/middlewares/handleIdValidation/handleIdValidation.js";
 
 const restaurantsRouter = Router();
 
 const restaurantController = new RestaurantController(Restaurant);
 
+restaurantsRouter.get("/", restaurantController.getRestaurants);
+
 restaurantsRouter.patch(
   "/visit-restaurant/:restaurantId",
+  handleIdValidation,
   restaurantController.toggleRestaurantById,
 );
-
-restaurantsRouter.get("/", restaurantController.getRestaurants);
 
 export default restaurantsRouter;
